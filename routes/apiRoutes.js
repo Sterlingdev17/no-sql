8adb0aa662520a992227cee75d554e5a1d9453de
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-05-01 21:23:02 -0400

    range routes done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -36,6 +36,18 @@ router.put("/api/workouts/:id", ({ body,params }, res) => {
       });
   });
 
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .then(dbWorkouts => {
+        res.json(dbWorkouts);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+
+
 
 
 module.exports = router;
